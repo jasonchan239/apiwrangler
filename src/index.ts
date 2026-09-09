02d0052e93d45d0173efd73ab2279670b7110920
jasonchan239 <jasonchan239@live.ca>
2026-09-09 17:34:31 -0400

    initial

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,38 @@
  */
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
+	async fetch(request: Request): Promise<Response> {
+		const url = new URL(request.url);
+		// Get /api/users
+		if (request.method === "GET" && url.pathname === "/api/users") {
+			return Response.json([
+				{id: 1, name: "John" },
+				{id: 2, name: "Jane" },
+			]);
+		}
+
+		// Get /api/users/:id
+		if (request.method === "GET" && url.pathname.startsWith("/api/users/")) {
+			const id = url.pathname.split("/").pop();
+			return Response.json({ 
+				id,
+				name: "John",
+			});
+		}
+		// POST /api/users
+		if (request.method === "POST" && url.pathname === "/api/users") {
+			const body = await request.json();
+			return Response.json(
+				{ 
+					message: "User Created",
+					user: body, 
+				},
+				{status: 201}
+			);
+		}
+		return Response.json(
+			{ error: "Route Not Found" },
+			{ status: 404 }
+		);
 	},
-} satisfies ExportedHandler<Env>;
+};
